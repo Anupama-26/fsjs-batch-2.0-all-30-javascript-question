53fc19180c131ddf87ed7ec88279114897dd6dd4
Anupama <121239113+Anupama-26@users.noreply.github.com>
2023-05-19 16:49:57 +0530

    Create split.js

---
+++ 5.question/split.js
@@ -0,0 +1,5 @@
+/*05. Declare a varibale and assign string value to it and then split it into an array using split() method
+ */
+let names='Anupama yadav';
+console.log(names.split());//['Anupama yadav']
+console.log(names.split(""));//['A','n', 'u','p','a','m','a','','y','a','d','a','v'];
