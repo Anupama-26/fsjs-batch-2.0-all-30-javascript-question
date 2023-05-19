eeb04e74db2817c24f9dff8da346be94d4637e00
Anupama <121239113+Anupama-26@users.noreply.github.com>
2023-05-19 16:51:15 +0530

    Create splitarray.js

---
+++ 6.question/splitarray.js
@@ -0,0 +1,4 @@
+/*06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array. */
+let splitArray="Facebook, Google,Microsoft, Apple, IBM,Oracle,Amazon";
+console.log(splitArray.split(""));////['F', 'a', 'c', 'e', 'b', 'o', 'o', 'k',',', ' ', 'G',',', ' ', 'M', 'i', 'c', 'r', 'o', 's', 'o', 'o', 'g', 'l', 'e','o', 'f', 't', ',', ' ', 'A', 'p', 'p','l', 'e', ',', ' ', 'I', 'B', 'M', ',',  ' ', 'O', 'r', 'a', 'c', 'l', 'e', ',',  ' ', 'A', 'm', 'a', 'z', 'o', 'n']
+
