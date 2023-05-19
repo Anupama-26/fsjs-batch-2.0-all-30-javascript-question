0eae807dc856584ed4db4509c1f1ed6600490952
Anupama <121239113+Anupama-26@users.noreply.github.com>
2023-05-19 17:02:49 +0530

    Create term.js

---
+++ 8.question/term.js
@@ -0,0 +1,7 @@
+/*08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+*/
+let vari="  Anu    "
+console.log(vari.length);//9
+//trim method
+console.log(vari.trim().length);//3
+console.log(vari.length);//9.
