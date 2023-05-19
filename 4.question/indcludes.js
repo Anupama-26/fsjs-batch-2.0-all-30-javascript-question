72ca68856ab4cf57a6d2b913e46d650286c7c3b2
Anupama <121239113+Anupama-26@users.noreply.github.com>
2023-05-19 16:48:29 +0530

    Create indcludes.js

---
+++ 4.question/indcludes.js
@@ -0,0 +1,5 @@
+/*04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+ */
+let words='string';
+// includes (value, index no of value)
+console.log(words.includes('string'));//true
