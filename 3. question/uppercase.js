a68e20bbf419441bb4790121dc565f1500164360
Anupama <121239113+Anupama-26@users.noreply.github.com>
2023-05-19 16:45:36 +0530

    Create uppercase.js

---
+++ 3. question/uppercase.js
@@ -0,0 +1,3 @@
+/*03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.*/
+let changeInUppercase='anupama yadav';
+console.log(changeInUppercase.toUpperCase());//ANUPAMA YADAV
