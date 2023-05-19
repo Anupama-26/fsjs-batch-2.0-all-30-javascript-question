005e5be0920ac38df2f5a25ce70b31ec15ab6cfe
Anupama <121239113+Anupama-26@users.noreply.github.com>
2023-05-19 16:43:35 +0530

    Create interpolistion.js

---
+++ 2 .question/interpolistion.js
@@ -0,0 +1,9 @@
+/*02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.*/
+let firstName='Anupama';
+let lastName='yadav';
+let maritalStatus='unmarried';
+let countryName='India';
+let age=26;
+//interpolation method
+console.log(`hi, my Name is ${firstName} ${lastName} and i am ${maritalStatus}i lived in ${countryName} i am ${age} Thank you`);
+//hi, my Name is Anupama yadav and i am unmarriedi lived in India i am 26 Thank you
